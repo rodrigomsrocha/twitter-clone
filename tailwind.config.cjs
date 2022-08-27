2ad89c8c448c802c82a9d6ac679964e1c2337da5
rodrigomsrocha <rodrigomtc82@gmail.com>
2022-08-26 23:37:34 -0300

    tailwind configured

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      md: "1022px",
+      sm: "500px",
+    },
+    colors: {
+      primary: "#000",
+      secondary: "#15181c",
+      search: "#202327",
+      white: "#fff",
+      gray: "#7a7a7a",
+      outline: "#2f3336",
+      retweet: "#00c068",
+      like: "#e8265e",
+      twitter: "#33a1f2",
+      "twitter-dark-hover": "#011017",
+      "twitter-light-hover": "#2c8ed6",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
